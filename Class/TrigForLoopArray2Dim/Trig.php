7b3ab5f557dd3d48f81e3c7c1cbd1c638f63255c
jm2292236 <jaime0422@live.com>
2014-09-27 21:23:30 -0700

    Class Update

--- Class/TrigForLoopArray2Dim/Trig.php
+++ Class/TrigForLoopArray2Dim/Trig.php
@@ -9,7 +9,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>For Loop Table</title>
+<title>For Loop Table with Arrays 2-Dim</title>
 </head>
 
 <body>
@@ -19,6 +19,20 @@
 	$angStrt=$_GET['angStrt'];
 	$angEnd=$_GET['angEnd'];
 	$angInc=$_GET['angInc'];
+	//Declare the arrays
+	$trigTab=array();
+	for($col=1;$col<=5;$col++){
+		$trigTab[$col]=array();
+	}
+	//Calculations for each parallel array
+	for($angle=$angStrt;$angle<=$angEnd;$angle+=$angInc){
+		//Calculate
+		$trigTab[1][$angle]=$angle;
+		$trigTab[2][$angle]=number_format($angle*atan(1)/45,4);
+		$trigTab[3][$angle]=number_format(sin($trigTab[2][$angle]),4);
+		$trigTab[4][$angle]=number_format(cos($trigTab[2][$angle]),4);
+		$trigTab[5][$angle]=number_format(tan($trigTab[2][$angle]),4);
+	};
 ?>
 <table width="200" border="1">
   <tr>
@@ -32,19 +46,15 @@
 	//For - Loop
 	for($angle=$angStrt;$angle<=$angEnd;$angle+=$angInc){
 		echo "<tr>";
-		echo "<td>$angle</td>";
-		//Calculate
-		$rad=number_format($angle*atan(1)/45,4);
-		$sine=number_format(sin($rad),4);
-		$cosine=number_format(cos($rad),4);
-		$tangent=number_format(tan($rad),4);
-		echo "<td>$rad</td>";
-		echo "<td>$sine</td>";
-		echo "<td>$cosine</td>";
-		echo "<td>$tangent</td>";
+		for($cols=1;$cols<=5;$cols++){
+			echo "<td>".$trigTab[$cols][$angle]."</td>";		
+		}
 		echo "</tr>";
-	};;;;;;
+	};
 ?>
 </table>
+<?php
+	echo "<p>The number of elements in the columns are ".count($degree)."</p>";
+?>
 </body>
 </html>
